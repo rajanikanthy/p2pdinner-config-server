ef545464009eb24da35b767e9ddf0f573da5f100
rajanikanthy <rajanikanthy@outlook.com>
2018-10-21 08:52:40 -0700

    updating yaml for kube deployment

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 export JAR_FILE=target/p2pdinner-config-server-0.0.1-SNAPSHOT.jar
 docker build -t p2pdinner-config-service .
-docker build --build-arg JAR_FILE=target/p2pdinner-config-server-0.0.1-SNAPSHOT.jar -t p2pdinner-config-service .
-
+docker build --build-arg JAR_FILE=target/p2pdinner-config-server-0.0.1-SNAPSHOT.jar -t gcr.io/glowing-vehicle-220016/p2pdinner-config-service .
+docker push
 # docker run -p 8888:8888 -t p2pdinner-config-service
